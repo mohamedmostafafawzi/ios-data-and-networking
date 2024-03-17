597ff3a4934c3d8f0a5128f0b8311a268bef0bd1
MohamedMostafaFawzi <mohamedmostafafawzi@outlook.com>
2024-03-17 16:36:34 +0200

    Background Downloads

--- RazeTunes/RazeTunes/AppDelegate.swift
+++ RazeTunes/RazeTunes/AppDelegate.swift
@@ -30,15 +30,39 @@
 /// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 /// THE SOFTWARE.
 
-import SwiftUI
+import UIKit
 
-@main
-struct AppMain: App {
-  @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+// MARK: App Delegate
+class AppDelegate: NSObject, UIApplicationDelegate {
+  // MARK: Properties
+  var backgroundCompletionHandler: (() -> Void)?
 
-  var body: some Scene {
-    WindowGroup {
-      ContentView()
+  // MARK: Application Delegate
+  // swiftlint:disable all
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication
+                                                                  .LaunchOptionsKey : Any]? = nil) -> Bool {
+    NotificationCenter.default.addObserver(self,
+                                           selector: #selector(backgroundSongDidDownload),
+                                           name: MutableSongDownloader.BackgroundSongDownloadDidFinish,
+                                           object: nil)
+
+    return true
+  }
+
+  func application(_ application: UIApplication,
+                   handleEventsForBackgroundURLSession identifier: String,
+                   completionHandler: @escaping () -> Void) {
+    // swiftlint:enable all
+    print("URLSession identifier: \(identifier)")
+
+    backgroundCompletionHandler = completionHandler
+  }
+
+  // MARK: Private Functions
+  @objc private func backgroundSongDidDownload() {
+    if let backgroundCompletionHandler = backgroundCompletionHandler {
+      backgroundCompletionHandler()
     }
   }
 }
