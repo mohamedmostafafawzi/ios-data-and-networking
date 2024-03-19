f18750d8e24da5a9555d039e9994fc5a3b4bfe51
MohamedMostafaFawzi <mohamedmostafafawzi@outlook.com>
2024-03-19 12:57:32 +0200

    Creating managed objects

--- RocketLaunches/RocketLaunches/Model/RocketLaunch+CoreDataClass.swift
+++ RocketLaunches/RocketLaunches/Model/RocketLaunch+CoreDataClass.swift
@@ -30,49 +30,7 @@
 /// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 /// THE SOFTWARE.
 
-import SwiftUI
+import CoreData
 
-struct LaunchCreateView: View {
-  // MARK: - Environment -
-  @Environment(\.dismiss) var dismiss
-
-  // MARK: - State -
-  @State var name: String = ""
-  @State var notes: String = ""
-  @State var isViewed = false
-  @State var launchDate = Date()
-  @State var launchpad: String = ""
-
-  var body: some View {
-    NavigationView {
-      Form {
-        Section {
-          TextField("Title", text: $name)
-          TextField("Launch Pad", text: $launchpad)
-          TextField("Notes", text: $notes)
-        }
-        Section {
-          DatePicker(selection: $launchDate, displayedComponents: .date) {
-            Text("Date")
-          }
-        }
-      }
-      .background(Color(.systemGroupedBackground))
-      .navigationBarTitle(Text("Create Event"), displayMode: .inline)
-      .navigationBarItems(trailing:
-        Button(action: {
-          dismiss()
-        }, label: {
-          Text("Save")
-            .fontWeight(.bold)
-        })
-      )
-    }
-  }
-}
-
-struct LaunchCreateView_Previews: PreviewProvider {
-  static var previews: some View {
-    LaunchCreateView()
-  }
-}
+@objc(RocketLaunch)
+public class RocketLaunch: NSManagedObject {}
--- RocketLaunches/RocketLaunches/Model/RocketLaunch+CoreDataProperties.swift
+++ RocketLaunches/RocketLaunches/Model/RocketLaunch+CoreDataProperties.swift
@@ -30,49 +30,12 @@
 /// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 /// THE SOFTWARE.
 
-import SwiftUI
+import CoreData
 
-struct LaunchCreateView: View {
-  // MARK: - Environment -
-  @Environment(\.dismiss) var dismiss
-
-  // MARK: - State -
-  @State var name: String = ""
-  @State var notes: String = ""
-  @State var isViewed = false
-  @State var launchDate = Date()
-  @State var launchpad: String = ""
-
-  var body: some View {
-    NavigationView {
-      Form {
-        Section {
-          TextField("Title", text: $name)
-          TextField("Launch Pad", text: $launchpad)
-          TextField("Notes", text: $notes)
-        }
-        Section {
-          DatePicker(selection: $launchDate, displayedComponents: .date) {
-            Text("Date")
-          }
-        }
-      }
-      .background(Color(.systemGroupedBackground))
-      .navigationBarTitle(Text("Create Event"), displayMode: .inline)
-      .navigationBarItems(trailing:
-        Button(action: {
-          dismiss()
-        }, label: {
-          Text("Save")
-            .fontWeight(.bold)
-        })
-      )
-    }
-  }
-}
-
-struct LaunchCreateView_Previews: PreviewProvider {
-  static var previews: some View {
-    LaunchCreateView()
-  }
+extension RocketLaunch {
+  @NSManaged public var name: String?
+  @NSManaged public var isViewed: Bool
+  @NSManaged public var launchDate: Date?
+  @NSManaged public var launchpad: String?
+  @NSManaged public var notes: String?
 }
